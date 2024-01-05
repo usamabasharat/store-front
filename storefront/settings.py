470aedf3c7d99b9cae631394bd3eb32785223c1a
Usama Basharat <dev.usamabasharat@gmail.com>
2024-01-05 03:22:31 -0800

    Created migrations for store, likes, and tags and conncet MYSQL

--- storefront/settings.py
+++ storefront/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,8 +89,11 @@ WSGI_APPLICATION = "storefront.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env('DB_NAME'),
+        "HOST": "localhost",
+        "USER": env('DB_USER'),
+        "PASSWORD": env('DB_PASSWROD'),
     }
 }
 
