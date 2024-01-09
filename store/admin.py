f164dee3865ca6076ff0860dfb6bf4350fa8c163
Usama Basharat <dev.usamabasharat@gmail.com>
2024-01-09 11:34:40 -0800

    Created admin panel with required models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,81 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.contrib.contenttypes.admin import GenericTabularInline
+from django.db.models.aggregates import Count
+from django.db.models.query import QuerySet
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
+from tags.models import TaggedItem
+from .models import Product, Customer, Collection
 
-# Register your models here.
+
+class InventoryListFilter(admin.SimpleListFilter):
+  title = 'Inventory'
+  parameter_name = 'inventory'
+
+  def lookups(self, request, model_admin):
+    return [(
+        '<10', 'Low'
+    )]
+
+  def queryset(self, request, queryset: QuerySet):
+    if self.value() == '<10':
+      return queryset.filter(inventory__lt=10)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+  autocomplete_fields = ['collection']
+  prepopulated_fields = {'slug': ['title']}
+  actions = ['clear_inventory']
+  list_display = ['title',
+                  'unit_price',
+                  'inventory_status',
+                  'collection_title']
+  list_editable = ['unit_price']
+  list_per_page = 10
+  list_select_related = ['collection']
+  list_filter = ['collection', 'last_update', InventoryListFilter]
+  search_fields = ['title__istartswith']
+
+  @admin.display(ordering='inventory')
+  def inventory_status(self, product):
+    if product.inventory < 10:
+      return 'Low'
+    return 'Ok'
+
+  @admin.action(description='Clear Inventory')
+  def clear_inventory(self, request, queryset):
+    updated_count = queryset.update(inventory=0)
+    self.message_user(
+        request,
+        f'{updated_count} products were updated successfully.',
+        messages.SUCCESS)
+
+  def collection_title(self, product):
+    return product.collection.title
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+  list_display = ['first_name', 'last_name', 'membership']
+  list_editable = ['membership']
+  list_per_page = 10
+  search_fields = ['first_name__istartswith', 'last_name__istartswith']
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+  list_display = ['title', 'products_count']
+  search_fields = ['title']
+
+  @admin.display(ordering='products_count')
+  def products_count(self, collection):
+    return format_html(
+        '<a href="{}">{}</a>',
+        reverse('admin:store_product_changelist') + '?' +
+        urlencode({'collection__id': str(collection.id)}),
+        collection.products_count
+    )
+
+  def get_queryset(self, request):
+    return super().get_queryset(request).annotate(products_count=Count('product'))
--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -11,16 +12,31 @@ class Collection(models.Model):
   featured_product = models.ForeignKey(
       'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
+  def __str__(self) -> str:
+    return self.title
+
+  class Meta:
+    ordering = ['title']
+
 
 class Product(models.Model):
   title = models.CharField(max_length=255)
-  slug = models.SlugField(default='-')
-  description = models.TextField()
-  unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+  slug = models.SlugField()
+  description = models.TextField(null=True, blank=True)
+  unit_price = models.DecimalField(
+      max_digits=6,
+      decimal_places=2,
+      validators=[MinValueValidator(1)])
   inventory = models.IntegerField()
   last_update = models.DateTimeField(auto_now=True)
   collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-  promotion = models.ManyToManyField(Promotion)
+  promotion = models.ManyToManyField(Promotion, blank=True)
+
+  def __str__(self) -> str:
+    return self.title
+
+  class Meta:
+    ordering = ['title']
 
 
 class Customer(models.Model):
@@ -41,6 +57,11 @@ class Customer(models.Model):
   membership = models.CharField(
       max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
+  def __str__(self) -> str:
+    return f'{self.first_name} {self.last_name}'
+
+  class Meta:
+    ordering = ['first_name']
 
 class Order(models.Model):
   PAYMENT_STATUS_PENDING = 'P'
