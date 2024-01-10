d35f179ad0543da04b2e62284eb587f34da8ca67
Usama Basharat <36359558+usamabasharat@users.noreply.github.com>
2024-01-10 17:11:20 +0500

    Implemented Product RESTful APIs (#2)

--- storefront/settings.py
+++ storefront/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "playground",
     "debug_toolbar",
     "store",
@@ -139,3 +140,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
