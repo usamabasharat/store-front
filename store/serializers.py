72b6ee2804f1754b5b642be529bd955d09322ab3
Usama Basharat <dev.usamabasharat@gmail.com>
2024-01-17 12:43:29 -0800

    Implemented ModelViewSet, Pagination, Sorting and Filtering

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 from rest_framework import serializers
-from store.models import Product, Collection
+from store.models import Product, Collection, Review
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -8,7 +8,7 @@ class CollectionSerializer(serializers.ModelSerializer):
     model = Collection
     fields = ['id', 'title', 'products_count']
 
-  products_count = serializers.IntegerField()
+  products_count = serializers.IntegerField(read_only=True)
 
 class ProductSerializer(serializers.ModelSerializer):
   class Meta:
@@ -24,3 +24,13 @@ class ProductSerializer(serializers.ModelSerializer):
 
   def calculate_tax(self, product: Product):
     return product.unit_price * Decimal(1.1)
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Review
+    fields = ['id', 'name', 'description', 'date']
+
+  def create(self, validated_data):
+    product_id = self.context['product_id']
+    return Review.objects.create(product_id=product_id, **validated_data)
--- store/views.py
+++ store/views.py
@@ -1,71 +1,44 @@
-from django.shortcuts import get_object_or_404
 from django.db.models import Count
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
-from .models import Product, Collection
-from .serializers import ProductSerializer, CollectionSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .models import Product, Collection, Review
+from .filters import ProductFilter
+from .pagination import DefaultPagination
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
 
-@api_view(['GET', 'POST'])
-def product_list(request):
-  if request.method == 'GET':
-    products = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(
-        products, many=True, context={'request': request})
-    return Response(serializer.data)
-  elif request.method == 'POST':
-    serializer = ProductSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+class ProductViewSet(ModelViewSet):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+  filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+  filterset_class = ProductFilter
+  search_fields = ['title', 'description']
+  ordering_fields = ['unit_price', 'updated_at']
+  pagination_class = DefaultPagination
+
+  def get_serializer_context(self):
+    return {'request': self.request}
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, id):
-  product = get_object_or_404(Product, pk=id)
-  if request.method == 'GET':
-    serializer = ProductSerializer(product, context={'request': request})
-    return Response(serializer.data)
-  elif request.method == 'PUT':
-    serializer = ProductSerializer(
-        product, data=request.data, context={'request': request})
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_200_OK)
-  elif request.method == 'DELETE':
-    product.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+class CollectionViewSet(ModelViewSet):
+  queryset = Collection.objects.annotate(
+      products_count=Count('products')).all()
+  serializer_class = CollectionSerializer
 
-
-@api_view(['GET', 'POST'])
-def collection_list(request):
-  if request.method == 'GET':
-    queryset = Collection.objects.annotate(
-        products_count=Count('products')).all()
-    serializer = CollectionSerializer(queryset, many=True)
-    return Response(serializer.data)
-  elif request.method == 'POST':
-    serializer = CollectionSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def collection_detail(request, pk):
-  collection = get_object_or_404(Collection.objects.annotate(
-      products_count=Count('products')), pk=pk)
-  if request.method == 'GET':
-    serializer = CollectionSerializer(collection)
-    return Response(serializer.data)
-  elif request.method == 'PUT':
-    serializer = CollectionSerializer(collection, data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_200_OK)
-  elif request.method == 'DELETE':
-    if collection.products.count() > 0:
+  def destroy(self, request, *args, **kwargs):
+    if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
       return Response({'error': 'This collection has associated product.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-    collection.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    return super().destroy(request, *args, **kwargs)
+
+
+class ReviewViewSet(ModelViewSet):
+  serializer_class = ReviewSerializer
+
+  def get_queryset(self):
+    return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
+  def get_serializer_context(self):
+    return {'product_id': self.kwargs['product_pk']}
