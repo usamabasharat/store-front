e5aef3a7fb99999d28e4e58f28bffcbe83b2f894
Usama Basharat <36359558+usamabasharat@users.noreply.github.com>
2024-01-18 01:44:49 +0500

    Implemented ModelViewSet, Pagination, Sorting and Filtering (#4)

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from rest_framework_nested import routers
 from . import views
 
-urlpatterns = [
-    path('products/', views.product_list),
-    path('products/<int:id>', views.product_detail),
-    path('collections/', views.collection_list),
-    path('collections/<int:pk>', views.collection_detail, name='collection-detail')
-]
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet, basename='products')
+router.register('collections', views.CollectionViewSet)
+
+products_router = routers.NestedDefaultRouter(
+    router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet,
+                         basename='product-reviews')
+
+urlpatterns = router.urls + products_router.urls
