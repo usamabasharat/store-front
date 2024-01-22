fe7b74d6974ea868a7d4164574fd398d24c8ce89
Usama Basharat <dev.usamabasharat@gmail.com>
2024-01-22 03:11:06 -0800

    Implemented custom user model and authentication

--- store/models.py
+++ store/models.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.contrib import admin
 from django.db import models
 from django.core.validators import MinValueValidator
 from uuid import uuid4
@@ -51,19 +53,24 @@ class Customer(models.Model):
       (MEMBERSHIP_GOLD, 'Gold')
   ]
 
-  first_name = models.CharField(max_length=255)
-  last_name = models.CharField(max_length=255)
-  email = models.EmailField(unique=True)
   phone = models.CharField(max_length=255)
   birth_date = models.DateField(null=True)
   membership = models.CharField(
       max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+  user = models.OneToOneField(
+      settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
   def __str__(self) -> str:
-    return f'{self.first_name} {self.last_name}'
+    return f'{self.user.first_name} {self.user.last_name}'
 
-  class Meta:
-    ordering = ['first_name']
+  @admin.display(ordering='user__first_name')
+  def first_name(self):
+    return self.user.first_name
+
+  @admin.display(ordering='user__last_name')
+  def last_name(self):
+    return self.user.last_name
+
 
 class Order(models.Model):
   PAYMENT_STATUS_PENDING = 'P'
@@ -79,6 +86,11 @@ class Order(models.Model):
   payment_status = models.CharField(
       max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
   customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+  class Meta:
+    permissions = [
+        ('cancel__order', 'Can cancel order')
+    ]
 
 
 class OrderItem(models.Model):
