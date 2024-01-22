e08f5a60b844adc2eb16b182749f753240202547
Usama Basharat <36359558+usamabasharat@users.noreply.github.com>
2024-01-22 16:12:04 +0500

    Implemented custom user model and authentication (#8)

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from store.admin import ProductAdmin
 from store.models import Product
 from tags.models import TaggedItem
+from core.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+  add_fieldsets = (
+      (
+          None,
+          {
+              "classes": ("wide",),
+              "fields": ("first_name", "last_name", "username", "email", "password1", "password2"),
+          },
+      ),
+  )
 
 
 class TaggedInline(GenericTabularInline):
