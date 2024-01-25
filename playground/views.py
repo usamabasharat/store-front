04ca1057f96e3db50f6e24dd7d6e4b85be2f54a6
Usama Basharat <dev.usamabasharat@gmail.com>
2024-01-25 07:34:36 -0800

    Uncommented code

--- playground/views.py
+++ playground/views.py
@@ -9,25 +9,27 @@ environ.Env.read_env()
 
 
 def say_hello(request):
+  # Background task
   notify_customer.delay('hello')
-  # try:
-  #   # Templated Email
-  #   message = BaseEmailMessage(
-  #       template_name='emails/hello.html', context={'name': 'Usama'})
-  #   message.send([env('EMAIL_TO')])
 
-  #   # Email with attachment
-  #   message = EmailMessage('Test Subject', 'Email Testing body',
-  #                          env('DEFAULT_FROM_EMAIL'), [env('EMAIL_TO')])
-  #   message.attach_file('playground/static/images/food.jpeg')
-  #   message.send()
+  try:
+    # Templated Email
+    message = BaseEmailMessage(
+        template_name='emails/hello.html', context={'name': 'Usama'})
+    message.send([env('EMAIL_TO')])
 
-  #   # Simple Email
-  #   send_mail('Test Subject', 'Email Testing body',
-  #             env('DEFAULT_FROM_EMAIL'), [env('EMAIL_TO')])
+    # Email with attachment
+    message = EmailMessage('Test Subject', 'Email Testing body',
+                           env('DEFAULT_FROM_EMAIL'), [env('EMAIL_TO')])
+    message.attach_file('playground/static/images/food.jpeg')
+    message.send()
 
-  #   # Email to Admins
-  #   mail_admins('Test admin subject', 'message', html_message='message')
-  # except BadHeaderError:
-  #   pass
+    # Simple Email
+    send_mail('Test Subject', 'Email Testing body',
+              env('DEFAULT_FROM_EMAIL'), [env('EMAIL_TO')])
+
+    # Email to Admins
+    mail_admins('Test admin subject', 'message', html_message='message')
+  except BadHeaderError:
+    pass
   return render(request, 'hello.html', {'name': 'Usama'})
